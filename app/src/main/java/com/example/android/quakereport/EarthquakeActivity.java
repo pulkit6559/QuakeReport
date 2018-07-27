b03ad58df1ca2c4f1f49c804981cddf635a6e27d
pulkit6559 <pulkitarora7.fas@gmail.com>
2018-07-28 03:23:25 +0530

    Added progress bar, empty view for no internet/data

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -92,47 +92,6 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderManag
     }
 
 
-    /*public static class EarthquakeLoader extends AsyncTaskLoader<ArrayList<earthquakes>> {
-
-
-        public EarthquakeLoader(Context context) {
-            super(context);
-        }
-
-        @Override
-        public ArrayList<earthquakes> loadInBackground() {
-
-            URL url = null;
-            try {
-                url = new URL(urlstring);
-            } catch (MalformedURLException exception) {
-                Log.e(LOG_TAG, "Error with creating URL", exception);
-            }
-
-
-            String JSONResponse = "";
-            ConnectivityManager connectivityManager =
-                    (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-            NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
-            try {
-                JSONResponse = makeHTTPRequest(url);
-            } catch (Exception e) {
-                Log.e("EarthQuakeACTIVITY", "********     JSONResponse CATCH      ********");
-
-            }
-            if (JSONResponse != null) {
-                EarthQuakeQuery earthQuakeQuery = new EarthQuakeQuery(JSONResponse);
-                ArrayList<earthquakes> earthquakes = earthQuakeQuery.extractEarthquakes();
-                return earthquakes;
-            }
-            return null;
-        }
-
-        @Override
-        protected void onStartLoading() {
-            forceLoad();
-        }
-    }*/
 
     public void updateUI(ArrayList<earthquakes> earthquakes) {
 
@@ -157,62 +116,5 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderManag
             }
         });
     }
-    /*
-    public static String makeHTTPRequest(URL url) {
-        String jsonResponse = "";
-        HttpURLConnection urlConnection = null;
-        InputStream inputStream = null;
-
-        try {
-
-            urlConnection = (HttpURLConnection) url.openConnection();
-            urlConnection.setRequestMethod("GET");
-            urlConnection.setReadTimeout(10000);
-            urlConnection.setConnectTimeout(15000);
-
-            urlConnection.connect();
-            Log.e("EarthQuakeActivity", "URLRESPONSE CODE");
-            if (urlConnection.getResponseCode() == 200) {
-                Log.e("EarthQuakeActivity", "URLRESPONSE CODE = 200");
-                inputStream = urlConnection.getInputStream();
-                jsonResponse = readFromStream(inputStream);
-            } else
-                Log.e("EarthQuakeActivity", "URLRESPONSE CODE != 200");
-            return jsonResponse;
-        } catch (Exception e) {
-            Log.e("EarthQuakeQuery.class", "********     MAKEHTTPREQUEST      ********", e.getCause());
-
-        } finally {
-            if (urlConnection != null)
-                urlConnection.disconnect();
-            if (inputStream != null) {
-                try {
-                    inputStream.close();
-                } catch (IOException e) {
-
-                }
-            }
-        }
-        return null;
-
-    }
-
-    private static String readFromStream(InputStream inputStream) throws IOException {
-        StringBuilder output = new StringBuilder();
-        if (inputStream != null) {
-            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
-            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-
-            String line = bufferedReader.readLine();
-            while (line != null) {
-                output.append(line);
-                line = bufferedReader.readLine();
-            }
-            Log.e("EarthQuakeActivity", output.toString());
-
-            return output.toString();
-        }
-        return null;
-    }*/
 
 }
